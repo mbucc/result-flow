b4e7c65a8481370090103d62ee5082c9364119cd
Mark Bucciarelli <mkbucc@gmail.com>
2020-02-04 19:05:20 -0500

    Fix JavaDoc errors

--- src/main/java/de/perschon/resultflow/Result.java
+++ src/main/java/de/perschon/resultflow/Result.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,16 +22,16 @@ import java.util.function.Function;
 /**
  * The Result type is an alternative way of chaining together functions in a
  * functional programming style while hiding away error handling structures such as
- * try-catch-blocks and conditionals.<br/>
- * 
- * Instead of adding a throws declaration to a function, the return type of the function
- * is instead set to Result<V, E> where V is the original return type, i.e. the
+ * try-catch-blocks and conditionals.
+ *
+ * <p>Instead of adding a throws declaration to a function, the return type of the function
+ * is instead set to Result&lt;V, E&gt; where V is the original return type, i.e. the
  * "happy case" and E is the error type, usually the Exception type or a String
- * if an error explanation is sufficient.<br/><br/>
- * 
+ * if an error explanation is sufficient.</p>
+ *
  * Example:
  * <pre>
- * public Result<Float, String> divide(int a, int b) {
+ * public Result&lt;Float, String&gt; divide(int a, int b) {
  *     if (b == 0) {
  *         return Result.err("Can't divide by zero!");
  *     } else {
@@ -39,7 +39,7 @@ import java.util.function.Function;
  *     }
  * }
  * </pre>
- *   
+ *
  * @param <V> The value type of the Result.
  * @param <E> The error type of the Result.
  */
@@ -75,20 +75,20 @@ public interface Result<V, E> {
 	 * @return see above.
 	 */
 	Optional<V> getValue();
-	
+
 	/**
 	 * Returns the error of this instance as an {@link Optional}. Returns Optional.empty()
 	 * if this is an Ok instance.
 	 * @return see above.
 	 */
 	Optional<E> getError();
-	
+
 	/**
 	 * Returns <code>true</code> if this instance represents an Ok value, false otherwise.
 	 * @return see above.
 	 */
 	boolean isOk();
-	
+
 	/**
 	 * Returns <code>true</code> if this instance represents an Err value, false otherwise.
 	 * @return see above
@@ -109,11 +109,11 @@ public interface Result<V, E> {
 	 * @throws ResultException if unwrap() is called on an Err value
 	 */
 	void expect(String message) throws ResultException;
-	
+
 	/**
 	 * If this is an Ok value, andThen() returns the result of the given {@link Function}.
 	 * Otherwise returns this.
-	 * 
+	 *
 	 * @param lambda The {@link Function} to be called with the value of this.
 	 * @param <U> The new value type.
 	 * @return see above.
@@ -127,11 +127,11 @@ public interface Result<V, E> {
 				return ret;
 			});
 	}
-	
+
 	/**
 	 * If this is an Ok value, map() returns the result of the given {@link Function}, wrapped
 	 * in a new Ok Result instance. Otherwise returns this.
-	 * 
+	 *
 	 * @param lambda The {@link Function} to call with the value of this.
 	 * @param <U> The new value type.
 	 * @return see above.
